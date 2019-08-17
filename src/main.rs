3566738edce168aced0ea69a3e3ce66a7fd9cb04
Andrew Rowles <andrew@rowles.io>
2019-08-17 14:07:32 -0400

    read request stream

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::io::prelude::*;
+use std::net::TcpStream;
 use std::net::TcpListener;
 
 
@@ -7,6 +9,15 @@ fn main() {
     for stream in listener.incoming() {
         let stream = stream.unwrap();
 
-        println!("Connection established");
+        handle_connection(stream);
     }
 }
+
+fn handle_connection(mut stream: TcpStream) {
+    let mut buffer = [0; 512];
+
+    stream.read(&mut buffer).unwrap();
+
+    println!("Request: {}", String::from_utf8_lossy(&buffer[..]));
+}
+
